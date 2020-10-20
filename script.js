f9d355d9a26079250f011334b888c349dff84028
sharjeelyunus <sharjeel924@gmail.com>
2020-10-20 19:51:39 +0500

    👌 IMPROVE: Items saved in local storage

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e) {
     e.preventDefault();
@@ -12,6 +12,7 @@ function addItem(e) {
 
     items.push(item);
     populateList(items, itemsList);
+    localStorage.setItem('items', JSON.stringify(items));
     this.reset();
 }
 
@@ -27,3 +28,5 @@ function populateList(plates = [], platesList) {
 }
 
 addItems.addEventListener('submit', addItem);
+
+populateList(items, itemsList);
